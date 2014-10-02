4bbeefcad929ad25709dd364e32b1871de1dd194
VOID001 <zhangjianqiu13@gmail.com>
2014-10-02 22:51:59 +0800

    Link the DataBase Server IN THE wxconfig NOT in functions

--- wxconfig.php
+++ wxconfig.php
@@ -7,4 +7,7 @@ $db_user=SAE_MYSQL_USER;
 $db_port=SAE_MYSQL_PORT;
 $db_pass=SAE_MYSQL_PASS;
 $RAWPOSTDATA=$_GLOBALS['HTTP_RAW_POST_DATA'];
+global $connstr;
+$connstr=mysql_connect($db_host.':'.$db_port,$db_user,$db_pass);
+echo mysql_error();
 global $fromUsername;
--- wxdbapi.php
+++ wxdbapi.php
@@ -2,7 +2,7 @@
 function DB_writeTo($id,$brand,$type)
 {
     //$connstr=mysql_connect($db_host.":".$db_port,$db_user,$db_pass);
-    $connstr = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+    //$connstr = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
     if(!$connstr) return -1;									//连接错误返回-1
    	$err=mysql_select_db(SAE_MYSQL_DB,$connstr);
     if(!$err) return -2;
@@ -17,7 +17,7 @@ function DB_writeTo($id,$brand,$type)
 
 function DB_ifExist($id)
 {
-    $connstr = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+    //$connstr = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
     $err=mysql_select_db(SAE_MYSQL_DB,$connstr);
     $SQLQUERY="SELECT * FROM reservation WHERE ID='".$id. "'";
     $ok=0;
